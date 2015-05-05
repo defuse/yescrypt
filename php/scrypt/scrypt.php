197931ad492ba7fe6b8046723a48c3b3f9924bb6
Taylor Hornby <havoc@defuse.ca>
2015-05-04 19:43:34 -0600

    Add super slow scrypt ref implementation

---
+++ php/scrypt/scrypt.php
@@ -0,0 +1,198 @@
+<?php
+
+/*
+ * This is a VERY SLOW reference implementation of plain scrypt. Don't use it!
+ *
+ * This software is Copyright (c) 2015 Taylor Hornby <havoc@defuse.ca>,
+ * and it is hereby released to the general public under the following terms:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ *
+ * There's ABSOLUTELY NO WARRANTY, express or implied.
+ *
+ */
+
+if (PHP_INT_MAX < 2 * 4294967296) {
+    /* We need to take integers up to twice 2^32 then mod them. */
+    /* XXX: This might actually work with 32-bit PHP, but we have to check the
+        guarantees when numbers up to that value are represented by floats. */
+    die('Your integers are too small.');
+}
+
+// XXX : error handling
+
+abstract class Scrypt {
+
+    public static function calculate($password, $salt, $N, $r, $p, $dkLen)
+    {
+        // XXX be more defensive (overflow)
+        $bytes = hash_pbkdf2('sha256', $password, $salt, 1, $p * 128 * $r, true);
+    
+        $B = array();
+        for ($i = 0; $i <= $p - 1; $i++) {
+            $B[$i] = str_split( substr($bytes, $i * 128 * $r, 128 * $r), 64 );
+        }
+    
+        for ($i = 0; $i <= $p - 1; $i++) {
+            $B[$i] = self::scryptROMix($r, $B[$i], $N);
+        }
+    
+        $new_salt = "";
+        for ($i = 0; $i <= $p - 1; $i++) {
+            $new_salt .= implode('', $B[$i]);
+        }
+    
+        return hash_pbkdf2('sha256', $password, $new_salt, 1, $dkLen, true);
+    }
+
+    /*
+     * The scryptROMix function.
+     * $r is the block size parameter.
+     * $B is a 2*r entry array of 64-byte strings.
+     * $N is the CPU/memory cost parameter. Must be a power of two larger than 1.
+     */
+    public static function scryptROMix($r, $B, $N)
+    {
+        // XXX: upper bound on N (yes it actually matters).
+        // XXX: check N is a power of 2.
+        // XXX: check the other shit
+        if (!is_int($r) || $r <= 0 || !is_array($B) || count($B) != 2*$r || !is_int($N) || $N <= 1) {
+            die('Bad parameters');
+        }
+    
+        $v = array();
+        $x = $B;
+        for ($i = 0; $i <= $N - 1; $i++) {
+            $v[$i] = $x;
+            $x = self::scryptBlockMix($r, $x);
+        }
+    
+        $t = array();
+        for ($i = 0; $i <= $N - 1; $i++) {
+            $j = self::integerify($r, $x) % $N; // & ($N - 1);
+            for ($k = 0; $k <= 2*$r - 1; $k++) {
+                $t[$k] = $x[$k] ^ $v[$j][$k];
+            }
+            $x = self::scryptBlockMix($r, $t);
+        }
+    
+        return $x;
+    }
+
+    public static function integerify($r, $B)
+    {
+        // XXX: this isn't the full range (doc limitation)
+        $last_block = $B[2*$r - 1];
+        return unpack("V", $last_block)[1];
+    }
+
+    /*
+     * The scryptBlockMix function.
+     * $r is the block size parameter.
+     * $B is a 2*r entry array of 64-byte strings.
+     * Returns a 2*r entry array of 64-byte strings.
+     */
+    public static function scryptBlockMix($r, $B)
+    {
+        if (!is_int($r) || $r <= 0 || !is_array($B) || count($B) != 2*$r) {
+            die('Bad parameters');
+        }
+    
+        // XXX: check each one's length.
+    
+        $x = $B[2*$r - 1];
+    
+        /* If we don't do this, it will be an associative array, and the implode()
+           won't come out in the right order. */
+        $y = array_fill(0, 2*$r, 0);
+    
+        for ($i = 0; $i <= 2*$r - 1; $i++) {
+            $y[$i] = 0;
+        }
+        for ($i = 0; $i <= 2*$r - 1; $i++) {
+            $t = $x ^ $B[$i];
+            $x = self::salsa20_8_core_binary($t);
+            if ($i % 2 == 0) {
+                $y[$i / 2] = $x;
+            } else {
+                $y[$r + ($i - 1)/2] = $x;
+            }
+        }
+        return $y;
+    }
+
+    public static function salsa20_8_core_binary($in)
+    {
+        // XXX: check input is 64-byte string.
+        $input_ints = array();
+        $output_ints = array();
+        for ($i = 0; $i < 16; $i++) {
+            // XXX slow
+            $input_ints[$i] = unpack("V", substr($in, $i*4, 4))[1];
+            $output_ints[$i] = 0;
+        }
+        self::salsa20_8_core_ints($input_ints, $output_ints);
+        $out = "";
+        for ($i = 0; $i < 16; $i++) {
+            $out .= pack("V", $output_ints[$i]);
+        }
+        return $out;
+    }
+
+    /*
+     * The reduced-round Salsa20 core function.
+     * Both parameters $in and $out must be a 16-entry array of ints.
+     * The result is left in $out.
+     */
+    public static function salsa20_8_core_ints($in, & $out)
+    {
+        if (!is_array($in) || count($in) != 16 || !is_array($out) || count($out) != 16) {
+            die('Bad parameters');
+        }
+    
+        $x = array();
+        for ($i = 0; $i < 16; $i++) {
+            if (!is_int($in[$i]) || !is_int($out[$i])) {
+                die('Bad parameters');
+            }
+            $x[$i] = $in[$i];
+        }
+    
+        for ($i = 8; $i > 0; $i -= 2) {
+             $x[ 4] ^= self::R(($x[ 0]+$x[12]) & 0xffffffff, 7);  $x[ 8] ^= self::R(($x[ 4]+$x[ 0]) & 0xffffffff, 9);
+             $x[12] ^= self::R(($x[ 8]+$x[ 4]) & 0xffffffff,13);  $x[ 0] ^= self::R(($x[12]+$x[ 8]) & 0xffffffff,18);
+             $x[ 9] ^= self::R(($x[ 5]+$x[ 1]) & 0xffffffff, 7);  $x[13] ^= self::R(($x[ 9]+$x[ 5]) & 0xffffffff, 9);
+             $x[ 1] ^= self::R(($x[13]+$x[ 9]) & 0xffffffff,13);  $x[ 5] ^= self::R(($x[ 1]+$x[13]) & 0xffffffff,18);
+             $x[14] ^= self::R(($x[10]+$x[ 6]) & 0xffffffff, 7);  $x[ 2] ^= self::R(($x[14]+$x[10]) & 0xffffffff, 9);
+             $x[ 6] ^= self::R(($x[ 2]+$x[14]) & 0xffffffff,13);  $x[10] ^= self::R(($x[ 6]+$x[ 2]) & 0xffffffff,18);
+             $x[ 3] ^= self::R(($x[15]+$x[11]) & 0xffffffff, 7);  $x[ 7] ^= self::R(($x[ 3]+$x[15]) & 0xffffffff, 9);
+             $x[11] ^= self::R(($x[ 7]+$x[ 3]) & 0xffffffff,13);  $x[15] ^= self::R(($x[11]+$x[ 7]) & 0xffffffff,18);
+             $x[ 1] ^= self::R(($x[ 0]+$x[ 3]) & 0xffffffff, 7);  $x[ 2] ^= self::R(($x[ 1]+$x[ 0]) & 0xffffffff, 9);
+             $x[ 3] ^= self::R(($x[ 2]+$x[ 1]) & 0xffffffff,13);  $x[ 0] ^= self::R(($x[ 3]+$x[ 2]) & 0xffffffff,18);
+             $x[ 6] ^= self::R(($x[ 5]+$x[ 4]) & 0xffffffff, 7);  $x[ 7] ^= self::R(($x[ 6]+$x[ 5]) & 0xffffffff, 9);
+             $x[ 4] ^= self::R(($x[ 7]+$x[ 6]) & 0xffffffff,13);  $x[ 5] ^= self::R(($x[ 4]+$x[ 7]) & 0xffffffff,18);
+             $x[11] ^= self::R(($x[10]+$x[ 9]) & 0xffffffff, 7);  $x[ 8] ^= self::R(($x[11]+$x[10]) & 0xffffffff, 9);
+             $x[ 9] ^= self::R(($x[ 8]+$x[11]) & 0xffffffff,13);  $x[10] ^= self::R(($x[ 9]+$x[ 8]) & 0xffffffff,18);
+             $x[12] ^= self::R(($x[15]+$x[14]) & 0xffffffff, 7);  $x[13] ^= self::R(($x[12]+$x[15]) & 0xffffffff, 9);
+             $x[14] ^= self::R(($x[13]+$x[12]) & 0xffffffff,13);  $x[15] ^= self::R(($x[14]+$x[13]) & 0xffffffff,18);
+        }
+    
+        for ($i = 0; $i < 16; $i++) {
+            $out[$i] = ($x[$i] + $in[$i]) & 0xffffffff;
+        }
+    }
+
+    /*
+     * Rotates a PHP integer $int right by $rot as though it were a 32-bit integer.
+     * Assumes PHP integers are at least 32 bits.
+     */
+    public static function R($int, $rot)
+    {
+        if (!is_int($int) || !is_int($rot) || $rot <= 0 || $rot >= 32) {
+            die('Bad parameters to rotate_int32');
+        }
+        return (($int << $rot) | (($int >> (32 - $rot)) /*& (pow(2, $rot) - 1)*/)) & 0xffffffff;
+    }
+    
+}
+
---
+++ php/scrypt/scrypt_test.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once('scrypt.php');
+
+$input = "   
+    7e 87 9a 21 4f 3e c9 86 7c a9 40 e6 41 71 8f 26
+    ba ee 55 5b 8c 61 c1 b5 0d f8 46 11 6d cd 3b 1d
+    ee 24 f3 19 df 9b 3d 85 14 12 1e 4b 5a c5 aa 32
+    76 02 1d 29 09 c7 48 29 ed eb c6 8d b8 b8 c2 5e";
+
+$output = "
+   a4 1f 85 9c 66 08 cc 99 3b 81 ca cb 02 0c ef 05
+   04 4b 21 81 a2 fd 33 7d fd 7b 1c 63 96 68 2f 29
+   b4 39 31 68 e3 c9 e6 bc fe 6b c5 b7 a0 6d 96 ba
+   e4 24 cc 10 2c 91 74 5c 24 ad 67 3d c7 61 8f 81";
+
+$input = str_replace([" ", "\n", "\r"], '', $input);
+$output = str_replace([" ", "\n", "\r"], '', $output);
+
+$input_binary = pack("H*",$input);
+$output_binary = Scrypt::salsa20_8_core_binary($input_binary);
+echo bin2hex($output_binary) . "\n";
+echo "$output\n";
+
+$scrypt = Scrypt::calculate("pleaseletmein", "SodiumChloride", 16384, 8, 1, 64);
+echo bin2hex($scrypt) . "\n";
+
+die();
+
+$b0 = "
+f7 ce 0b 65 3d 2d 72 a4 10 8c f5 ab e9 12 ff dd
+           77 76 16 db bb 27 a7 0e 82 04 f3 ae 2d 0f 6f ad
+           89 f6 8f 48 11 d1 e8 7b cc 3b d7 40 0a 9f fd 29
+           09 4f 01 84 63 95 74 f3 9a e5 a1 31 52 17 bc d7
+";
+
+$b1 = "
+89 49 91 44 72 13 bb 22 6c 25 b5 4d a8 63 70 fb
+           cd 98 43 80 37 46 66 bb 8f fc b5 bf 40 c2 54 b0
+           67 d2 7c 51 ce 4a d5 fe d8 29 c9 0b 50 5a 57 1b
+           7f 4d 1c ad 6a 52 3c da 77 0e 67 bc ea af 7e 89
+";
+
+$b0 = hex2bin(str_replace([" ", "\n", "\r"], '', $b0));
+$b1 = hex2bin(str_replace([" ", "\n", "\r"], '', $b1));
+
+$out = Scrypt::scryptBlockMix(1, [$b0, $b1]);
+echo bin2hex($out[0]) . "\n";
+echo bin2hex($out[1]) . "\n";
+
+$b = "
+f7 ce 0b 65 3d 2d 72 a4 10 8c f5 ab e9 12 ff dd
+       77 76 16 db bb 27 a7 0e 82 04 f3 ae 2d 0f 6f ad
+       89 f6 8f 48 11 d1 e8 7b cc 3b d7 40 0a 9f fd 29
+       09 4f 01 84 63 95 74 f3 9a e5 a1 31 52 17 bc d7
+       89 49 91 44 72 13 bb 22 6c 25 b5 4d a8 63 70 fb
+       cd 98 43 80 37 46 66 bb 8f fc b5 bf 40 c2 54 b0
+       67 d2 7c 51 ce 4a d5 fe d8 29 c9 0b 50 5a 57 1b
+       7f 4d 1c ad 6a 52 3c da 77 0e 67 bc ea af 7e 89
+";
+$b = hex2bin(str_replace([" ", "\n", "\r"], '', $b));
+$b = str_split($b, 64);
+$out = Scrypt::scryptROMix(1, $b, 16);
+echo bin2hex(implode('', $out));
