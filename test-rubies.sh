e1dd950b98816da55c928eb3af177704bbdc6e8c
Taylor Hornby <havoc@defuse.ca>
2015-06-23 06:24:44 -0600

    Try another travis-ci thing

--- test-rubies.sh
+++ test-rubies.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
-rvm use 1.8.7
-./test-ruby.sh
+rvm 1.8.7 exec ./test-ruby.sh
 
-rvm use 1.9.2
-./test-ruby.sh
+rvm 1.9.2 exec ./test-ruby.sh
 
-rvm use 1.9.3
-./test-ruby.sh
+rvm 1.9.3 exec ./test-ruby.sh
 
-rvm use 2.0.0
-./test-ruby.sh
+rvm 2.0.0 exec ./test-ruby.sh
 
-rvm use 2.1
-./test-ruby.sh
+rvm 2.1 exec ./test-ruby.sh
 
-rvm use 2.2
-./test-ruby.sh
+rvm 2.2 exec ./test-ruby.sh
 
-rvm use jruby-18mode
-./test-ruby.sh
+rvm jruby-18mode exec ./test-ruby.sh
 
-rvm use jruby-19mode
-./test-ruby.sh
+rvm jruby-19mode exec ./test-ruby.sh
 
-rvm use rbx-2
-./test-ruby.sh
+rvm rbx-2 exec ./test-ruby.sh
