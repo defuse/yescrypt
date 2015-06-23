a96be5a68dbbbd21e113c9db38bd1be572918c2e
Taylor Hornby <havoc@defuse.ca>
2015-06-23 07:01:00 -0600

    Add sudo to PHP apt-gets, comment all ruby tests to test

--- test-phps.sh
+++ test-phps.sh
@@ -4,10 +4,10 @@ set -e
 
 # http://packages.ubuntu.com/search?keywords=php5
 
-apt-get remove php
-apt-get install php5=5.5.12+dfsg-2ubuntu4.4
+sudo apt-get remove php
+sudo apt-get install php5=5.5.12+dfsg-2ubuntu4.4
 ./test-php.sh
 
-apt-get remove php
-apt-get install php5=5.6.4+dfsg-4ubuntu6
+sudo apt-get remove php
+sudo apt-get install php5=5.6.4+dfsg-4ubuntu6
 ./test-php.sh
--- test-rubies.sh
+++ test-rubies.sh
@@ -18,11 +18,11 @@ rvm install 2.2
 #
 #rvm 1.9.3 exec ./test-ruby.sh
 #
-rvm 2.0.0 exec ./test-ruby.sh
-
-rvm 2.1 exec ./test-ruby.sh
-
-rvm 2.2 exec ./test-ruby.sh
+#rvm 2.0.0 exec ./test-ruby.sh
+#
+#rvm 2.1 exec ./test-ruby.sh
+#
+#rvm 2.2 exec ./test-ruby.sh
 
 #rvm jruby-18mode exec ./test-ruby.sh
 #
