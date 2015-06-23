573290c28d92177c1cc1486c42e9e267a64319e4
Taylor Hornby <havoc@defuse.ca>
2015-06-23 06:31:18 -0600

    Try installing the ruby versions ourselves

--- test-rubies.sh
+++ test-rubies.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+set -e
+
+rvm install 1.8.7
+rvm install 1.9.2
+rvm install 1.9.3
+rvm install 2.0.0
+rvm install 2.1
+rvm install 2.2
+rvm install jruby-18mode
+rvm install jruby-19mode
+rvm install rbx-2
+
 rvm 1.8.7 exec ./test-ruby.sh
 
 rvm 1.9.2 exec ./test-ruby.sh
