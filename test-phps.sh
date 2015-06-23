f41335b44418acce00114a6a3a41b6f4417aa12b
Taylor Hornby <havoc@defuse.ca>
2015-06-23 07:14:41 -0600

    Fix apt-get remove command

--- test-phps.sh
+++ test-phps.sh
@@ -4,10 +4,10 @@ set -e
 
 # http://packages.ubuntu.com/search?keywords=php5
 
-sudo apt-get remove php
+sudo apt-get remove php5
 sudo apt-get install php5=5.5.12+dfsg-2ubuntu4.4
 ./test-php.sh
 
-sudo apt-get remove php
+sudo apt-get remove php5
 sudo apt-get install php5=5.6.4+dfsg-4ubuntu6
 ./test-php.sh
