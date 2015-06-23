104e5cfc12cdc552c19a8defd2404286d5128a71
Taylor Hornby <havoc@defuse.ca>
2015-06-23 07:27:44 -0600

    Try to wildcard

--- test-phps.sh
+++ test-phps.sh
@@ -7,9 +7,9 @@ set -e
 sudo apt-get update
 
 sudo apt-get remove php5
-sudo apt-get install php5=5.5.12+dfsg-2ubuntu4.4
+sudo apt-get install php5=5.5\*
 ./test-php.sh
 
 sudo apt-get remove php5
-sudo apt-get install php5=5.6.4+dfsg-4ubuntu6
+sudo apt-get install php5=5.6\*
 ./test-php.sh
