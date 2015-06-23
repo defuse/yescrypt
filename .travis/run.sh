c33b3f9e01413ae015ff3e8fcb34e5b1315e8039
Taylor Hornby <havoc@defuse.ca>
2015-06-23 13:32:38 -0600

    Add more versions

--- .travis/run.sh
+++ .travis/run.sh
@@ -4,9 +4,11 @@ set -e
 
 case "${TORUN}" in
     ruby)
+        echo "Running the Ruby tests..."
         ./test-ruby.sh
         ;;
     php)
+        echo "Running the PHP tests..."
         ./test-php.sh
         ;;
 esac
